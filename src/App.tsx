340ca0f33f915963d8925f57161145b17180af0d
arsen056 <vurdihanov@gmail.com>
2023-01-28 12:40:50 +0300

    feat: add SuperCheckbox and SuperInputText components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {SuperButton} from "./components/SuperButton";
+import {SuperCheckbox} from "./components/SuperCheckbox/SuperCheckbox";
+import {SuperInputText} from "./components/SuperInputText/SuperInputText";
 
 function App() {
+
+  const [checked, setChecked] = useState<boolean>(false)
+
   return (
     <div className="App" >
         <SuperButton>Default</SuperButton>
         <SuperButton xType={'red'}>Default</SuperButton>
         <SuperButton xType={'secondary'}>Default</SuperButton>
         <SuperButton disabled>Default</SuperButton>
+
+        <SuperCheckbox checked={checked} onChangeChecked={setChecked}/>
+
+        <SuperInputText/>
     </div>
   );
 }
