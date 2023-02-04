d3f48c0c35d3c47ef2b5ff1b39ffdafde64bd8b5
arsen056 <vurdihanov@gmail.com>
2023-02-04 23:51:52 +0300

    feat: layout profile

--- src/app/appReducer.ts
+++ src/app/appReducer.ts
@@ -1,4 +1,14 @@
-const initState = {status: 'idle'}
+const initState: AppStateType = {
+  status: 'idle',
+  isLoggedIn: false
+}
+
+export type AppStateType = {
+  status: AppStatusType,
+  isLoggedIn: boolean
+}
+
+export type AppStatusType = 'idle' | 'loading' | 'error' | 'success'
 
 export const appReducer = (state=initState, action: AppActionsType) => {
   switch (action.type) {
--- src/features/Profile/Profile.tsx
+++ src/features/Profile/Profile.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import s from './Profile.module.css'
+import avatar from '../../assets/avatar.png'
 
 export const Profile = () => {
   return (
-    <div>
-      This Profile
+    <div className={s.profileWrapper}>
+      <div id='profile-container' className={s.profileContainer}>
+        <h2 id='profile-title' className={s.title}>Personal Information</h2>
+        <div id='profile-avatar' className={s.avatarWrapper}>
+          <img src={avatar} alt="avatar"/>
+        </div>
+
+        <h3 id='nickname' className={s.name}>
+          Ivan
+          <img src="" alt=""/>
+        </h3>
+
+        <div id='email' className={s.email}>j&johnson@gmail.com</div>
+
+        <button id='profile-logout-btn' className={s.btn}>Log out</button>
+      </div>
     </div>
   );
 };
