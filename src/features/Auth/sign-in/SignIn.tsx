48e64ea98e0af55a8141ba891b7c4e8070be9555
Ekaterina <kattekly@mail.ru>
2023-02-04 21:44:53 +0300

    eslint

--- src/features/Auth/sign-in/SignIn.tsx
+++ src/features/Auth/sign-in/SignIn.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 export const SignIn = () => {
-  return (
-    <div>
-      Login
-      <label htmlFor="email">E-mail</label>
-      <input name='email' type="text"/>
-      <label htmlFor="password">Password</label>
-      <input name='password' type="password"/>
-    </div>
-  );
+    return (
+        <div>
+            Login
+            <label htmlFor="email">E-mail</label>
+            <input name='email' type="text"/>
+            <label htmlFor="password">Password</label>
+            <input name='password' type="password"/>
+        </div>
+    );
 };
